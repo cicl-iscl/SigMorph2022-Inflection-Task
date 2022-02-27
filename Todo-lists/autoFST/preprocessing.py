4840f18920e9b5bc3dd33e893612fd4e35070890
Jingwen Li <865581101@qq.com>
2022-02-27 03:03:34 +0100

    to-do?

---
+++ Todo-lists/autoFST/preprocessing.py
@@ -0,0 +1,89 @@
+"""
+"""
+
+from pathlib import Path
+import pandas as pd
+from paradigm import Paradigm
+
+task = "part1"
+# task = "part2"
+data_dir = Path("2021Task0", "part1", "development_languages")
+
+
+def get_files(data_dir=None, iso=None, test=False):
+    if iso is not None:
+        files = [x for x in data_dir.iterdir() if x.stem.startswith(iso)]
+    else:
+        files = data_dir.iterdir()
+    if test is False:
+        files = [x for x in files if x.suffix != ".test"]
+    return files
+
+
+def group_by_lemma(files):
+    dfs = (pd.read_csv(f, sep="\t", names=["lemma", "form", "tag"]) for f in files)
+    data = pd.concat(dfs)
+    grouped = data.groupby("lemma")
+
+    return grouped
+
+
+def sort_tags(group):
+    return group.sort_values(by=["tag"], ascending=True)
+
+
+def decompose_group(group):
+    return group["lemma"].iloc[0], group["form"].tolist(), group["tag"].tolist()
+
+
+def main():
+    # print(get_files(data_dir))
+    files = get_files(data_dir, "deu")
+    print(files)
+    print()
+    print()
+    grouped = group_by_lemma(files)
+    lemmata = grouped.groups.keys()
+    groups = [grouped.get_group(x) for x in grouped.groups]
+    print(f"{len(lemmata)} lemmas")
+    print(groups[0])
+    print()
+    print()
+    umsteigen = grouped.get_group("umsteigen")
+    print(umsteigen)
+    print()
+    print()
+    umsteigen = sort_tags(umsteigen)
+    print(umsteigen)
+    trennbar = any(" " in x for x in umsteigen["form"].tolist())
+    print(trennbar)  # potentially useful for data augmentation
+    lemma, forms, tags = decompose_group(umsteigen)
+    print(lemma)
+    print(forms)
+    print(tags)
+
+    # To-Do:
+    # - need LCS? generate variable stem representation like s t ei:{ei|ie} g, associate with tag???
+    # - s t ei:TENSE g en:NUMBER+PERSON ???
+    # - generate rules by comparing tags and forms within a paradigm
+    # - (prioritize paradigms with more entries)
+    # ==== Language-specific? ====
+    # - define C, V
+    # - if trennbar, strip particle, merge two paradigms, hallucinate more separable verbs
+
+    # Use case from paradigm.py
+    # paradigm = Paradigm(lemma, forms)
+
+    # print()
+    # print(paradigm.alignment)
+    # print()
+    # print(paradigm.segments)
+    # print()
+
+    # for form in forms:
+    #     print(paradigm.form2segments(form))
+    #     print()
+
+
+if __name__ == "__main__":
+    main()
